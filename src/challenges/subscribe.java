88a601b0c35d90ef3b6bb75b3d9e98a5751e7713
debugsoul07 <suhrid838@gmail.com>
2025-06-20 08:28:22 +0530

    Variables java

---
+++ src/challenges/subscribe.java
@@ -0,0 +1,12 @@
+
+
+public class subscribe{
+
+
+    public  static  void main(String [] args){
+
+        System.out.println("subscribe karoo  Guys ");
+    }
+
+
+}
---
+++ src/goodmorning.java
@@ -0,0 +1,12 @@
+
+
+public  class goodmorning{
+
+    public static void main(String[] args){
+
+        System.out.println("good morning");
+    }
+
+
+
+}
