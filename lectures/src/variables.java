5d4b4e7960ce2bf22cd3809c7319b19a0600cb7f
debugsoul07 <suhrid838@gmail.com>
2025-06-21 07:40:36 +0530

    variables and basics

---
+++ lectures/src/variables.java
@@ -0,0 +1,31 @@
+
+public class variables {
+    public static void main(String[] args) {
+
+
+
+//         data types
+
+        int myno= 23;
+        System.out.println(myno);
+
+
+    boolean areyouveg= false;
+
+        System.out.println(areyouveg);
+
+
+
+        float myfloat = 5;
+        System.out.println(myfloat);
+
+
+        double mygn = 4.5;
+        System.out.println(mygn);
+
+
+
+        String myname = "debugsoul";
+        System.out.println(myname);
+    }
+}
